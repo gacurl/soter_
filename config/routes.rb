837861f4b15a17cb531f1ad2335b2b91063f809b
gacurl <greg.a.curl@gmail.com>
2023-10-08 11:16:08 -0400

    update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'activities/index'
-  get 'activities/new'
-  get 'activities/edit'
+  get "activites", to: "activities#index"
+
+  root to: "activities#index"
+  # get 'activities/index'
+  # get 'activities/new'
+  # get 'activities/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
