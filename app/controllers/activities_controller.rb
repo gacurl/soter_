3582dca2f4b3ff67f020cf53a4a9fcb301048910
gacurl <greg.a.curl@gmail.com>
2023-10-08 10:44:08 -0400

    adding activity index logic

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,7 @@
 class ActivitiesController < ApplicationController
   # you also need a form in this activites folder
   def index
-    
+    @activities = ["shuffleboard", "knitting", "going to the movies", "playing cards"]
   end
 
   def new
