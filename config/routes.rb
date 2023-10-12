f3ded1a112f3e97443b61365830196d84a52433e
gacurl <greg.a.curl@gmail.com>
2023-10-12 18:42:36 -0400

    add root route to static_pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'activities/index'
   get 'activities/new'
   get 'activities/edit'
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
 
-  # root 'activities/index'
+  
 end
