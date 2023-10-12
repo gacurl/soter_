db33d3eed696605df421a0b7cf7c724cf9c86709
gacurl <greg.a.curl@gmail.com>
2023-10-12 18:49:40 -0400

    add test for root route

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "| Soter Cares"
   end
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
