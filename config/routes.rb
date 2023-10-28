a2a25e58b0e1398ee33a0e88df78ab2a32534d7e
gacurl <greg.a.curl@gmail.com>
2023-10-28 17:09:27 -0400

    change routes to paths

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   get 'activities/new'
   get 'activities/edit'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get "/help",    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
   
 end
