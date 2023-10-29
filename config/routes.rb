8bff1acddf1a3c8632fe528d3696b8fc02a82fc4
gacurl <greg.a.curl@gmail.com>
2023-10-29 10:29:07 -0400

    add helper paths and integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   get 'activities/new'
   get 'activities/edit'
 
-  get "/help",    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-
+  get "/help",    to: "static_pages#help"
+  get "/about",   to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
   
 end
