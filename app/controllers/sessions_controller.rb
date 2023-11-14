c231680d78e2e0ac91575d5db1811d11494f55f3
gacurl <greg.a.curl@gmail.com>
2023-11-14 16:30:07 -0500

    add create and destroy actions to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Login the user and redirect to the user's show page
+    #else
+      #Create an error message
+    end
+    render 'new', status: :unprocessable_entity
+  end
+
+  def destroy
+  end
 end
