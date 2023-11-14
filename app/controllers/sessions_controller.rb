c615f1582e3e0b04e3ee97754a3778fb168e9107
gacurl <greg.a.curl@gmail.com>
2023-11-14 16:32:47 -0500

    add flash danger message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #Login the user and redirect to the user's show page
-    #else
+    else
       #Create an error message
+      flash[:danger] = 'Invalid email/password combination'
     end
     render 'new', status: :unprocessable_entity
   end
