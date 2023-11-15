6b57017510a022b8746ee313fc9dd7f1448739ed
gacurl <greg.a.curl@gmail.com>
2023-11-14 19:20:02 -0500

    add current_user method to sessions_helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #Login the user and redirect to the user's show page
+      reset_session #this resets the session BEFORE the user logs in preventing session fixation attack
+      log_in user
+      redirect_to user
     else
       #Create an error message
       flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
     end
-    render 'new', status: :unprocessable_entity
   end
 
   def destroy
