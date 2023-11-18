7c7f60a30b0f51c56680e5a6c11ccad787591425
gacurl <greg.a.curl@gmail.com>
2023-11-18 11:39:05 -0500

    add integration tests for remember me functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ApplicationRecord
 
   #Remembers true if given token matches the digest
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
