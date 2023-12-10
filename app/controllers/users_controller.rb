fbb007a32883e308b1a2f455bbc8f232c9b0c6c3
gacurl <greg.a.curl@gmail.com>
2023-12-10 09:52:19 -0500

    friendly forwarding users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,13 +44,16 @@ class UsersController < ApplicationController
                                   :password_confirmation)
     end
 
+    # Confirms a logged in user
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url, status: :see_other
       end
     end
 
+    #Confirms the Correct User
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class UsersEditTest < ActionDispatch::IntegrationTest
+  
   def setup
     @user = users(:michael)
   end
@@ -16,10 +17,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
-    log_in_as(@user)
+  test "successful edit with friendly formatting" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name: name,
