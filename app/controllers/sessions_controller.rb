45b01db2aa2ed777d159beaf2869f58caeb6934b
gacurl <greg.a.curl@gmail.com>
2024-01-07 09:36:34 -0500

    add authenticate method to sessions_controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session #this resets the session BEFORE the user logs in preventing session fixation attack
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      log_in @user
-      redirect_to forwarding_url || @user
-    else
-      #Create an error message
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session #this resets the session BEFORE the user logs in preventing session fixation attack
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        log_in user
+        redirect_to forwarding_url || user
+      else
+        message = "Account not activated"
+        message += "Check your email for the activation link."
+        #Create an error message
+        flash[:warning] = message
+        redirect_to root_url
+      end
+    else        
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
